22b87cb9149c68137096ec7d9db5bfdfea9a8019
phanvinh0526 <phanvinh0526@gmail.com>
2019-05-12 11:15:41 +1000

    Handle condition in React with Boolean

--- src/App.js
+++ src/App.js
@@ -10,31 +10,75 @@ class App extends Component {
       { name: 'Manu', age: 29},
       { name: 'Stephanie', age: 26}
     ],
-    otherState: 'Some other value'
+    otherState: 'Some other value',
+    showPerson: false
   }
 
-  switchNameHandler = () => {
-    // console.log("Was clicked!");
+  switchNameHandler = (newName) => {
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian'; => instead of using setState function in Component
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 38},
+        { name: newName, age: 38},
         { name: 'Manu', age: 39},
         { name: 'Stephanie', age: 36}
       ]
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 38},
+        { name: event.target.value, age: 39},
+        { name: 'Stephanie', age: 36}
+      ]
+    })
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow});
+  }
+
   render() {
+    // Inline styles
+    const style1={
+      backgroundColor: 'yellow',
+      font: 'inherit',
+      border: '2x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+    const style2={
+      backgroundColor: 'organ',
+      font: 'inherit',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, Hello React</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <h1>Use State property</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name="Manu" age="29">My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button 
+          style={style1}
+          onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
+        <button
+          style={style2}
+          onClick={this.togglePersonHandler}>Switch Condition Handler</button>
+        {
+          this.state.showPerson === true ?
+        <div>
+          <Person name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangeHandler}>My hobbies: Racing</Person>
+          <Person name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div> : null
+        }
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Person.css';
 
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old</p>
             <p>My name is {props.name} and i am {props.age} years old</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+            <br/>
         </div>
     );
 };
